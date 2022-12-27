dc25a62d670921a9d094212b0269054475df8278
Maxim <maksim_mikhaylov1810@mail.ru>
2022-12-27 17:30:08 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,11 +8,33 @@ export const homeWorkReducer = (state: any, action: any): any => { // need to fi
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            return [...state].sort(function (a, b) {
+
+                if(action.payload === 'up'){
+                    if (a.name > b.name) {
+                        return 1;
+                    }
+                    if (a.name < b.name) {
+                        return -1;
+                    }
+                    // a должно быть равным b
+                    return 0;
+                } else {
+                    if (a.name < b.name) {
+                        return 1;
+                    }
+                    if (a.name > b.name) {
+                        return -1;
+                    }
+                    // a должно быть равным b
+                    return 0;
+                }
+
+            })// need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return [...state].filter(el => el.age >= 18) // need to fix
         }
         default:
             return state
