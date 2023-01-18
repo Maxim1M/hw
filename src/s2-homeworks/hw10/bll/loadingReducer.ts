ef4ebd4475acac03d54262dabedf5a7bfa0e9e4a
Maxim <maksim_mikhaylov1810@mail.ru>
2023-01-18 17:38:38 +0300

    HW10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,10 +2,14 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): typeof state=> { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING': {
+                return {
+                    isLoading: action.isLoading
+                }
+            }
         default:
             return state
     }
